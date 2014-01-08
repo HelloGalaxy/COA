a7de5641d64bacee4d3630c277c7d28a1e76724d
PU <puchengcheng@outlook.com>
2014-01-08 10:01:59 +0100

    1. The class diagram not finished yet.

---
+++ diffusion_service/diffusion-service/src/main/java/fr/istic/coa/diffusion_service/App.java
@@ -0,0 +1,13 @@
+package fr.istic.coa.diffusion_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
